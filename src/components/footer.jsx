fa69fb26ba0549b891b151a697c8056a24a549fa
JuanAngelBi <105744962+JuanAngelBi@users.noreply.github.com>
2022-10-07 16:18:20 -0300

    Header y Footer
    Ya estan los elementos

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,7 +1,9 @@
 export default function Footer() {
      return (
          <div>
-        
+            <footer>
+
+            </footer>
          </div>
      )
 }
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,6 +1,9 @@
 export default function Header() {
      return (
          <div>
+            <nav className="bg-red-200">
+
+            </nav>
             
          </div>
      )
