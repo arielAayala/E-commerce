72bd71a9570642d9277025189a14d122402a7266
arielAayala <ariel170802@gmail.com>
2022-10-14 17:29:55 -0300

    contextAuth y routes

--- src/context/authContext.js
+++ src/context/authContext.js
@@ -1,15 +1,15 @@
-import Context from "./context"
-import { React,useState,useEffect } from "react";
-import {createUserWithEmailAndPassword, GoogleAuthProvider, signInWithEmailAndPassword, onAuthStateChanged,signOut,signInWithPopup,sendPasswordResetEmail} from "firebase/auth"
+import AuthContext from "./context"
+import React,{useState} from "react";
+import {createUserWithEmailAndPassword, GoogleAuthProvider, signInWithEmailAndPassword,signOut,signInWithPopup,sendPasswordResetEmail} from "firebase/auth"
 import {auth} from "../services/firebase"
 
 
 
-export default function AuthContext(props){
+export default function AuthProvider(props){
     const{children} = props
 
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    const [user, setUser] = useState(null);
+
+    const [user, setUser] = useState(null); 
 
     const logIn = (email,password)=> signInWithEmailAndPassword(auth,email,password)
 
@@ -25,14 +25,14 @@ export default function AuthContext(props){
 
     const logOut = () => signOut(auth)
 
-    useEffect(()=>{
-        onAuthStateChanged((auth,user)=>{
-            setUser(user)
-        })
-    },[]) 
 
 
     return (
-        <Context.Provider value={{user,logIn,register,resetPassword,logInGoogle,logOut}}>{children}</Context.Provider>
+        <>
+        <AuthContext.Provider value={{user,logIn,register,resetPassword,logInGoogle,logOut}}>
+            {children}
+        </AuthContext.Provider>
+        </>
+        
     )
 }
--- src/routes/App.js
+++ src/routes/App.js
@@ -1,14 +1,18 @@
-import AuthContext from "../context/context"
+import AuthProvider from "../context/authContext"
 import {Route,Routes} from "react-router-dom"
 import Home from "../containers/home";
 import Login from "../containers/login"
+import Register from "../containers/register";
 export default function App() {
   return (
-
-      <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/login" element={<Login/>} />
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/register" element={<Register/>}/>
+        </Routes>
+      </AuthProvider>
+      
 
   );
 }
