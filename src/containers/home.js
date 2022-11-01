c3af9f4dd92d28036f43bdfdf3129760046fc556
arielAayala <ariel170802@gmail.com>
2022-11-01 19:39:01 -0300

    carrousel

--- src/containers/home.js
+++ src/containers/home.js
@@ -4,8 +4,8 @@ import Carrousel from "../components/carrousel";
 export default function Home() {
     return(
         <>
-            <div className="w-75">
-                <Header></Header>
+            <Header></Header>
+            <div className="w-100 m-auto">
                 <Carrousel></Carrousel>
             </div>
         </>
