7245528137a948891d38011e3fdf1e1629e12891
Santi Handel <santiagohandel@gmail.com>
2022-11-02 18:11:52 -0300

    sigo el footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import logo from "../assets/static/logo.png"
 export default function Footer (){
     return (
     <div>
         <footer className="text white py-4 bg-dark ">
             <div className="container">
-            <nav>
+            <nav className="row">
                 <Link to="/" className="col-12 col-md-3 d-flex aling-items-center justyfy-content-center">
                     Home
                 </Link>
             </nav>
+    
             </div>
         </footer>
     </div>
