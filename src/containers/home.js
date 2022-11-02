8db65c4f3374248afd79a2821ca19d84de71a93f
Fabricio Valdez <fabriciovaldezz41@gmail.com>
2022-11-02 11:07:55 -0300

    q
    :anguished:

--- src/containers/home.js
+++ src/containers/home.js
@@ -1,12 +1,18 @@
 import Header from "../components/header"
 import Carrousel from "../components/carrousel";
+import Slider1 from "../assets/static/slider_1.jpg"
+import Slider2 from "../assets/static/slider_2.jpg"
+import Slider3 from "../assets/static/slider_3.jpg"
 
 export default function Home() {
+
+const images = [Slider1,Slider2,Slider3]
+
     return(
         <>
             <Header></Header>
             <div className="w-100 m-auto">
-                <Carrousel></Carrousel>
+                <Carrousel images={images} autoPlay={true}></Carrousel>
             </div>
         </>
     )
